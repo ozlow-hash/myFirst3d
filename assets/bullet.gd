7c064b26d6e2be2f19e2b391eee5967bd345b9f3
ozlow-hash <austinlegg@gmail.com>
2026-07-08 20:58:20 -0700

    started bullet shooting

--- assets/bullet.gd
+++ assets/bullet.gd
@@ -1,6 +1,7 @@
-extends Node3D
+extends CharacterBody3D
 
-#var direction = 
+var bullet_velocity: Vector3
+var damage: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,4 +10,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#position += transform.basis * Vector3(0, 0, -bullet_velocity) * delta
+	pass
+
+func _physics_process(delta: float) -> void:
 	pass
